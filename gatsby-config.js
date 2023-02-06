720f86440e75311c469a92362a508a88c9d7a743
o-ibiloye <o.ibiloye@fangocloud.co.uk>
2023-02-06 15:42:43 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `fango-website-frontend`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
